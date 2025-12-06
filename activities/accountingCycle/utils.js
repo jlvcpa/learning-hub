366065ab281558e4555d1aa54c1f8626f84da049
jlvcpa <dpi.acct@gmail.com>
2025-12-06 19:33:10 +0800

    Update utils.js

--- activities/accountingCycle/utils.js
+++ activities/accountingCycle/utils.js
@@ -1,6 +1,6 @@
 // Constants and Logic Helpers for Accounting Cycle
 
-export const APP_VERSION = "Version: 2025-12-06 13:20 PST";
+export const APP_VERSION = "Version: 2025-12-06 19:15 PST";
 
 export const EQUITY_CAUSES = ['', 'Increase in Capital', 'Decrease in Capital', 'Increase in Drawings', 'Decrease in Drawings', 'Increase in Income', 'Decrease in Income', 'Increase in Expense', 'Decrease in Expense'];
 
