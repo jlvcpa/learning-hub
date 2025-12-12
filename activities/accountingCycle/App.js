c5acf3d94c6e0a3086bd7b0bd397b010929d26bf
jlvcpa <dpi.acct@gmail.com>
2025-12-13 02:51:51 +0800

    Update App.js

--- activities/accountingCycle/App.js
+++ activities/accountingCycle/App.js
@@ -1,3 +1,6 @@
+// -----------------
+// --- App.js ------
+// -----------------
 import React, { useState, useCallback, useEffect } from 'https://esm.sh/react@18.2.0';
 import htm from 'https://esm.sh/htm';
 import { Book, Check, RefreshCw, ArrowLeft } from 'https://esm.sh/lucide-react@0.263.1';
