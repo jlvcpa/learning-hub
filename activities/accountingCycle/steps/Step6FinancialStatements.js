9b4feb47f3d2bf8793c2a068af9d863f7bf816fa
jlvcpa <dpi.acct@gmail.com>
2025-12-13 00:31:30 +0800

    Update Step6FinancialStatements.js

--- activities/accountingCycle/steps/Step6FinancialStatements.js
+++ activities/accountingCycle/steps/Step6FinancialStatements.js
@@ -1,3 +1,4 @@
+// --- Step6FinancialStatements.js ---
 import React, { useState, useMemo, useEffect } from 'https://esm.sh/react@18.2.0';
 import htm from 'https://esm.sh/htm';
 import { Table, Trash2, Plus, List, ChevronDown, ChevronRight } from 'https://esm.sh/lucide-react@0.263.1';
