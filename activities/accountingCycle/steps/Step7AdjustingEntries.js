f530c19e7edf56b38cd9004ac9eb0ac615d2fd82
jlvcpa <dpi.acct@gmail.com>
2025-12-13 00:34:29 +0800

    Update import statements in Step7AdjustingEntries.js

--- activities/accountingCycle/steps/Step7AdjustingEntries.js
+++ activities/accountingCycle/steps/Step7AdjustingEntries.js
@@ -1,3 +1,4 @@
+// --- Step7AdjustingEntries.js ---
 import React, { useState, useMemo } from 'https://esm.sh/react@18.2.0';
 import htm from 'https://esm.sh/htm';
 import { Book, Check, X, ChevronDown, ChevronRight, Table, Trash2, Plus } from 'https://esm.sh/lucide-react@0.263.1';
